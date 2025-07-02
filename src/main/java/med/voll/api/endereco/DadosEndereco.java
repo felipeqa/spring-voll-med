3d02569ea631893acea2964eb9f8f1a7bd308c7c
Luis Felipe Rodrigues de Oliveira <felipe_rodriguesx@hotmail.com>
2025-07-01 22:04:43 -0300

    implementando as validações com bean annotations

--- src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -1,10 +1,27 @@
 package med.voll.api.endereco;
 
-public record DadosEndereco(String logradouro,
-                            String numero,
-                            String complemento,
-                            String bairro,
-                            String cidade,
-                            String uf,
-                            String cep) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosEndereco(
+
+        @NotBlank
+        String logradouro,
+
+        @NotBlank
+       String bairro,
+
+        @NotBlank
+        String cidade,
+
+        @NotBlank
+        String uf,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{8}")
+        String cep,
+        
+        String numero,
+
+        String complemento) {
 }
