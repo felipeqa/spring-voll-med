b1bce009520d91fe19553cc9a08cea2dbd434198
Luis Felipe Rodrigues de Oliveira <felipe_rodriguesx@hotmail.com>
2025-07-07 21:17:25 -0300

    add o id no DTO

--- src/main/java/med/voll/api/medico/DadosDetalhadoRetornoMedico.java
+++ src/main/java/med/voll/api/medico/DadosDetalhadoRetornoMedico.java
@@ -2,9 +2,9 @@ package med.voll.api.medico;
 
 import med.voll.api.endereco.Endereco;
 
-public record DadosDetalhadoRetornoMedico(String nome, String email, String crm, String telefone, Especialidade especialidade, Endereco endereco) {
+public record DadosDetalhadoRetornoMedico(Long id, String nome, String email, String crm, String telefone, Especialidade especialidade, Endereco endereco) {
 
     public DadosDetalhadoRetornoMedico(Medico medico) {
-        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(), medico.getEndereco());
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(), medico.getEndereco());
     }
 }
