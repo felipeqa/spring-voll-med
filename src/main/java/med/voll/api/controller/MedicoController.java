52f58777062d37e77ae700105d8d75d42789a4b8
Luis Felipe Rodrigues de Oliveira <felipe_rodriguesx@hotmail.com>
2025-07-07 21:17:08 -0300

    controller post, atualizando o retorno

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/medicos")
@@ -19,8 +20,14 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dadosCadastroMedico) {
-        repository.save(new Medico(dadosCadastroMedico));
+    public ResponseEntity<DadosDetalhadoRetornoMedico> cadastrar(@RequestBody @Valid DadosCadastroMedico dadosCadastroMedico, UriComponentsBuilder uriBulder) {
+        var medico = new Medico(dadosCadastroMedico);
+        repository.save(medico);
+
+        var uri = uriBulder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosDetalhadoRetornoMedico(medico));
+
     }
 
     //sem paginação
@@ -38,8 +45,9 @@ public class MedicoController {
 
     //alterando o default para 10 e ordenação por nome
     @GetMapping
-    public Page<DadosListagemMedico> listarMedicos(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    public ResponseEntity<Page<DadosListagemMedico>> listarMedicos(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+//        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+        return ResponseEntity.ok(repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new));
     }
 
     @PutMapping
